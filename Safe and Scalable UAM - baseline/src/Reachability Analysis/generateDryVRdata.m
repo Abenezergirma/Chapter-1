52ef9d65d9aad0ec7dff5fc3f925d30b20ae02d9
abenezergirma <abengirma1@gmail.com>
2023-09-08 15:53:18 -0400

    streamline reachset related scripts

--- Safe and Scalable UAM - baseline/src/Reachability Analysis/generateDryVRdata.m
+++ Safe and Scalable UAM - baseline/src/Reachability Analysis/generateDryVRdata.m
@@ -1,34 +1,46 @@
-clear 
+% This script is used to generate the reachSet.mat file that is used to plot
+% the simulation traces and the reachable sets in my papers  
+clear all; close all; clc;
 
-north = 10;
-east = 10;
-height = 10;
-chi = 0.3;
-gamma = 0.2;
-phi = 0.2;
-Va = 15;
+% Define constants
+north   = 10;
+east    = 10;
+height  = 10;
+chi     = 0.3;
+gamma   = 0.2;
+phi     = 0.2;
+Va      = 15;
+
+% Initial states setup
 initialStates = [north, east, height, chi, gamma, phi, Va];
 
+% Start timing
 tic
-model = DryVR(0.1,300); % [x, y, z] pos of the vehicle
 
-% initialStates = initialStates;
+% Initialize model
+model = DryVR(0.1,300); % [x, y, z] position of the vehicle
+
+% Generate traces
 traces = GenerateTraces(model, 'fixedwing_vector', initialStates);
-t = 0:0.01:5;
-B = traces;
-B(:,:,8) = repmat(t,1,1,1000); %DO this before the simulation
-Traces = B(:,:,[8,1:7]);
-traces = permute(Traces, [2,1,3]);
-Traces = traces(1:50:1000,5:end,:);
-first = Traces(1,:,:);
-mid = Traces(10,:,:);
-Traces(1,:,:) = mid;
-Traces(10,:,:) = first;
-% Traces = traces(:,3:end,:);
+
+% Post-process and reformat traces
+t = 0:0.01:10;
+% Extend and reorder the traces matrix
+traces(:,:,8) = repmat(t,1,1,1000);
+
+% Slicing, permuting, and sampling every 50 from start to 1000, while ignoring the first 4 columns
+Traces = permute(traces(:,:,[8,1:7]), [2,1,3]);
+Traces = Traces(1:50:1000, 5:end, :);
+
+% Swap the first and tenth slices
+Traces([1 10],:,:) = Traces([10 1],:,:);
 
 
-initialRadii = computeInitialRadii(model,Traces);
+% Compute initial radii, discrepancy parameters, and reach tube
+initialRadii = computeInitialRadii(model, Traces);
 discrepancyParameters = computeDiscrepancyParameters(model, Traces, initialRadii);
 reachTube = getReachtube(model, Traces, initialRadii, discrepancyParameters);
+
+% End timing and save results
 toc
-save('output')
+save('reachSet')
